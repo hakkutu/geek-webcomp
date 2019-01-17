64d88b01151574c2eec0ade857570444c00e5379
shimo-yuki <c0116138b7@edu.teu.ac.jp>
2019-01-17 15:33:54 +0900

    [add] cdのindex.htmlとshow.html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,7 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-	if artist_signed_in?
-		def after_sign_in_path_for(resource)
-	  		artists_artist_path(current_artist)
-		end
 
-	elsif admin_signed_in?
-		def after_sign_in_path_for(resource)
-  			admins_admins_path(current_admin)
-		end
-	elsif user_signed_in?
-		def after_sign_in_path_for(resource)
-  			cds_index_path(current_user)
-		end
- 	end
   protected
 
   def configure_permitted_parameters
--- app/controllers/cds_controller.rb
+++ app/controllers/cds_controller.rb
@@ -1,7 +1,14 @@
 class CdsController < ApplicationController
   def index
+  	@cds = Cd.all
   end
 
   def show
+  	@cd = Cd.find(params[:id])
+  	@discs = @cd.discs
+  	@songs = @cd.songs
+  	@user_comment = User_comment.new
+  	@user_comments = User_comment.all
+
   end
 end
