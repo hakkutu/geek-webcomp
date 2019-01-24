6297027c7f606f3626e4282210f3b79d8b723fbc
shimo-yuki <c0116138b7@edu.teu.ac.jp>
2019-01-24 12:18:59 +0000

    user_users first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,25 @@
 class UsersController < ApplicationController
   def edit
+  	@user = User.find(params[:id])
   end
 
   def show
+  	@user = User.find(params[:id])
+  	@user_artists = User.artists
+  	@user_orderlists = User.orderlists
+
   end
 
   def update
+  	@user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 
   def destroy
   end
+    private
+    def user_params
+       params.require(:user).permit(:name,:email,:postcode,:address,:phone_number,:kana_name)
+    end
 end
