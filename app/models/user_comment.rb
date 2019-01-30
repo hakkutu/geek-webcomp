37675f3d34b224564b3bb8a078836e15cb83f080
shimo-yuki <38341920+shimo-yuki@users.noreply.github.com>
2019-01-30 19:51:38 +0900

    Update user_comment.rb

--- app/models/user_comment.rb
+++ app/models/user_comment.rb
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
+
 class UserComment < ApplicationRecord
 	belongs_to :user
 	belongs_to :cd
 end
-=======
-class UserComment < ApplicationRecord
-	belongs_to :user
-	belongs_to :comment
-end
->>>>>>> master
