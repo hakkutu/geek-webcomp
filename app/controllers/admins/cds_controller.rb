cb322b580f9dc33aa59893c5dc5d9b39e17c2619
YoshidaTakeshi <yoshida.takeshi.infratop@gmail.com>
2019-02-04 21:50:45 +0900

    create controller

--- app/controllers/admins/cds_controller.rb
+++ app/controllers/admins/cds_controller.rb
@@ -1,2 +1,27 @@
 class Admins::CdsController < ApplicationController
+  def index
+    @cds = Cd.all
+  end
+
+  def show
+    @cd = Cd.find(params[:id])
+  end
+
+  def edit
+    @cd = Cd.find(params[:id])
+  end
+
+  def update
+    cd = Cd.find(params[:id])
+    cd.update
+    redirect_to admins_cd_path(cd)
+  end
+
+  def destroy
+    cd = Cd.find(params[:id])
+    cd.destroy
+    redirect_to admins_cds_path
+  end
+
+
 end
