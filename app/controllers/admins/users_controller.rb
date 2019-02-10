0808f1ade2dfc1cc47df630c0396639d237b399b
shimo-yuki <c0116138b7@edu.teu.ac.jp>
2019-02-10 07:14:14 +0000

    admins_users

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -2,4 +2,32 @@ class Admins::UsersController < ApplicationController
 	def index
 		@users = User.all
 	end
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  	@user_artists = @user.artists
+  	@user_orderlists = @user.orderlists
+  end
+
+  def update
+  	@user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to admins_users_path
+  end
+  def unsubscribe
+
+  end
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admins_users_path
+  end
+    private
+    def user_params
+       params.require(:user).permit(:name,:email,:postcode,:address,:phone_number,:kana_name)
+    end	
+
 end
