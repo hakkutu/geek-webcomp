86226b8c5d4bdca31a0e473d2c9f5b35bc716d70
Shintaro0311 <nakaoka.shintaro.infratop@gmail.com>
2019-02-19 11:38:03 +0000

    check now

--- app/models/orderlist.rb
+++ app/models/orderlist.rb
@@ -1,5 +1,5 @@
 class Orderlist < ApplicationRecord
-	has_many :orderlist_cds
-	has_many :cds,through: :orderlist_cds
+	has_many :orderlist_cds, dependent: :destroy
+	has_many :cds,through: :orderlist_cds, dependent: :destroy
 	belongs_to :user
 end
